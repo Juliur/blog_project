1cda529602f0813d65327485bf3e28b8de7f32c6
Julia <juliayrkina@gmail.com>
2019-11-09 12:54:19 +0200

    adaptivity for nav added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './common/Style/reset.css';
 import './common/Style/base.css';
+import './common/Style/adaptivity.css'
 
 import Header from './App/Header/Header';
 import Main from './App/Main/Main';import Carousel from './App/Carousel/Carousel';
