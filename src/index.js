283413e28e573b3968fcef3d86af4508ac49d491
Julia <juliayrkina@gmail.com>
2019-12-05 17:27:02 +0200

    hashrouter changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './store/rootReducer';
@@ -10,7 +10,7 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>, document.getElementById('root'));
