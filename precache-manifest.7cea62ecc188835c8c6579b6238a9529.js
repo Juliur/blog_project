a0aeadea494247c343fb2ab93c5c3c2bda95d3a0
Julia <juliayrkina@gmail.com>
2019-12-06 18:32:01 +0200

    Updates

---
+++ precache-manifest.7cea62ecc188835c8c6579b6238a9529.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6814b3ea98c933db3af3849d03419bac",
+    "url": "/blog_project/index.html"
+  },
+  {
+    "revision": "3e64ac47237d820af2f6",
+    "url": "/blog_project/static/css/2.a30423a2.chunk.css"
+  },
+  {
+    "revision": "4854013d4147b82193c8",
+    "url": "/blog_project/static/css/main.d8c324bc.chunk.css"
+  },
+  {
+    "revision": "3e64ac47237d820af2f6",
+    "url": "/blog_project/static/js/2.432137a3.chunk.js"
+  },
+  {
+    "revision": "4854013d4147b82193c8",
+    "url": "/blog_project/static/js/main.e8ff7284.chunk.js"
+  },
+  {
+    "revision": "c82cd10981a57d4a31f3",
+    "url": "/blog_project/static/js/runtime-main.c8fc8088.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/blog_project/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/blog_project/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/blog_project/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/blog_project/static/media/slick.f97e3bbf.svg"
+  }
+]);
